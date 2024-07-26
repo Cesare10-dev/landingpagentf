824b03e7b1aa1c0d4c7fb66c5f1884e12593218d
Cesare10-dev <cesare1087@gmail.com>
2024-07-26 20:19:08 +0200

    testim

---
+++ src/components/Testimonials.tsx
@@ -0,0 +1,77 @@
+import avatar1 from "@/assets/img1.png";
+
+const testimonials = [
+  {
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, modi facilis aspernatur deserunt incidunt vero.",
+    imageSrc: avatar1.src,
+    name: "John Doe",
+    username: "@johndoe",
+  },
+  {
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, modi facilis aspernatur deserunt incidunt vero.",
+    imageSrc: avatar1.src,
+    name: "John Doe",
+    username: "@johndoe",
+  },
+  {
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, modi facilis aspernatur deserunt incidunt vero.",
+    imageSrc: avatar1.src,
+    name: "John Doe",
+    username: "@johndoe",
+  },
+  {
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, modi facilis aspernatur deserunt incidunt vero.",
+    imageSrc: avatar1.src,
+    name: "John Doe",
+    username: "@johndoe",
+  },
+  {
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, modi facilis aspernatur deserunt incidunt vero.",
+    imageSrc: avatar1.src,
+    name: "John Doe",
+    username: "@johndoe",
+  },
+  {
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, modi facilis aspernatur deserunt incidunt vero.",
+    imageSrc: avatar1.src,
+    name: "John Doe",
+    username: "@johndoe",
+  },
+  {
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, modi facilis aspernatur deserunt incidunt vero.",
+    imageSrc: avatar1.src,
+    name: "John Doe",
+    username: "@johndoe",
+  },
+  {
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, modi facilis aspernatur deserunt incidunt vero.",
+    imageSrc: avatar1.src,
+    name: "John Doe",
+    username: "@johndoe",
+  },
+  {
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, modi facilis aspernatur deserunt incidunt vero.",
+    imageSrc: avatar1.src,
+    name: "John Doe",
+    username: "@johndoe",
+  },
+];
+
+const Testimonials = () => {
+  return (
+    <section className="bg-white">
+      <div className="container">
+        <div className="flex justify-center">
+          <div className="tag">Testimonials</div>
+        </div>
+        <h2 className="section-title mt-5">What our users say</h2>
+        <p className="section-description mt-5">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
+          recusandae voluptatum possimus, incidunt eum officiis?
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Testimonials;
