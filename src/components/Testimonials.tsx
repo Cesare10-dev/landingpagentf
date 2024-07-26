23f22d3b6463c0741751576880ed9e9be1cb9fab
Cesare10-dev <cesare1087@gmail.com>
2024-07-27 00:22:47 +0200

    testim boxes

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -1,4 +1,5 @@
 import avatar1 from "@/assets/img1.png";
+import Image from "next/image";
 
 const testimonials = [
   {
@@ -57,6 +58,10 @@ const testimonials = [
   },
 ];
 
+const firstColumn = testimonials.slice(0, 3);
+const secondColumn = testimonials.slice(3, 6);
+const thirdColumn = testimonials.slice(6, 9);
+
 const Testimonials = () => {
   return (
     <section className="bg-white">
@@ -69,6 +74,28 @@ const Testimonials = () => {
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
           recusandae voluptatum possimus, incidunt eum officiis?
         </p>
+        <div className="flex flex-col gap-6 mt-10">
+          {firstColumn.map(({ text, imageSrc, name, username }) => (
+            <div className="card">
+              <div>{text}</div>
+              <div className="flex items-center gap-2 mt-5">
+                <Image
+                  src={imageSrc}
+                  alt={name}
+                  width={40}
+                  height={40}
+                  className="w-10 h-10 rounded-full object-cover"
+                />
+                <div className="flex flex-col">
+                  <div className="font-medium tracking-tight leading-5">
+                    {name}
+                  </div>
+                  <div className="tracking-tight leading-5">{username}</div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );
