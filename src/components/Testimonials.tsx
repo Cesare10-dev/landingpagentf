3fa9f89e5c1d667945c8e3fb4d718b26b12cbf4e
Cesare10-dev <cesare1087@gmail.com>
2024-07-27 17:26:13 +0200

    testim done

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -1,5 +1,6 @@
 import avatar1 from "@/assets/img1.png";
 import Image from "next/image";
+import { twMerge } from "tailwind-merge";
 
 const testimonials = [
   {
@@ -62,39 +63,61 @@ const firstColumn = testimonials.slice(0, 3);
 const secondColumn = testimonials.slice(3, 6);
 const thirdColumn = testimonials.slice(6, 9);
 
+const TestimonialsColumn = (props: {
+  className?: string;
+  testimonials: typeof testimonials;
+}) => (
+  <div
+    className={twMerge(
+      "flex flex-col gap-6 mt-10 [mask-image:linear-gradient(to_bottom,transparent,black_25%,black_75%,transparent)]",
+      props.className
+    )}
+  >
+    {props.testimonials.map(({ text, imageSrc, name, username }) => (
+      <div className="card">
+        <div>{text}</div>
+        <div className="flex items-center gap-2 mt-5">
+          <Image
+            src={imageSrc}
+            alt={name}
+            width={40}
+            height={40}
+            className="w-10 h-10 rounded-full object-cover"
+          />
+          <div className="flex flex-col">
+            <div className="font-medium tracking-tight leading-5">{name}</div>
+            <div className="tracking-tight leading-5">{username}</div>
+          </div>
+        </div>
+      </div>
+    ))}
+  </div>
+);
+
 const Testimonials = () => {
   return (
     <section className="bg-white">
-      <div className="container">
-        <div className="flex justify-center">
-          <div className="tag">Testimonials</div>
+      <div className="container mx-auto">
+        <div className="section-heading">
+          <div className="flex justify-center">
+            <div className="tag">Testimonials</div>
+          </div>
+          <h2 className="section-title mt-5">What our users say</h2>
+          <p className="section-description mt-5">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
+            recusandae voluptatum possimus, incidunt eum officiis?
+          </p>
         </div>
-        <h2 className="section-title mt-5">What our users say</h2>
-        <p className="section-description mt-5">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
-          recusandae voluptatum possimus, incidunt eum officiis?
-        </p>
-        <div className="flex flex-col gap-6 mt-10">
-          {firstColumn.map(({ text, imageSrc, name, username }) => (
-            <div className="card">
-              <div>{text}</div>
-              <div className="flex items-center gap-2 mt-5">
-                <Image
-                  src={imageSrc}
-                  alt={name}
-                  width={40}
-                  height={40}
-                  className="w-10 h-10 rounded-full object-cover"
-                />
-                <div className="flex flex-col">
-                  <div className="font-medium tracking-tight leading-5">
-                    {name}
-                  </div>
-                  <div className="tracking-tight leading-5">{username}</div>
-                </div>
-              </div>
-            </div>
-          ))}
+        <div className="flex justify-center gap-6">
+          <TestimonialsColumn testimonials={firstColumn} />
+          <TestimonialsColumn
+            testimonials={secondColumn}
+            className="hidden md:flex"
+          />
+          <TestimonialsColumn
+            testimonials={thirdColumn}
+            className="hidden lg:flex"
+          />
         </div>
       </div>
     </section>
