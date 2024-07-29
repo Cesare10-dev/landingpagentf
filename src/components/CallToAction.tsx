9a1f27c64ad6fb28563bb30f508897728fb79595
Cesare10-dev <cesare1087@gmail.com>
2024-07-29 19:14:45 +0200

    cat animation

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -1,11 +1,25 @@
+"use client";
 import { ArrowLongRightIcon } from "@heroicons/react/24/solid";
 import cog from "@/assets/3dcog.png";
 import cyl from "@/assets/3dcyl.png";
 import Image from "next/image";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
 
 const CallToAction = () => {
+  const sectionRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: sectionRef,
+    offset: ["start end", "end start"],
+  });
+
+  const translateY = useTransform(scrollYProgress, [0, 1], [150, -150]);
+
   return (
-    <section className="bg-gradient-to-b from-white to-[#D2DCFF] py-24 overflow-x-clip">
+    <section
+      ref={sectionRef}
+      className="bg-gradient-to-b from-white to-[#D2DCFF] py-24 overflow-x-clip"
+    >
       <div className="container mx-auto">
         <div className="section-heading relative">
           <h2 className="section-title">Sign up for free today</h2>
@@ -13,17 +27,23 @@ const CallToAction = () => {
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt
             obcaecati ullam, vero necessitatibus quod sequi!
           </p>
-          <Image
-            src={cog}
+          <motion.img
+            src={cog.src}
             alt="cog"
             width={360}
             className="absolute -left-[350px] -top-[137px]"
+            style={{
+              translateY,
+            }}
           />
-          <Image
-            src={cyl}
+          <motion.img
+            src={cyl.src}
             alt="cyl"
             width={360}
             className="absolute -right-[331px] -top-[19px]"
+            style={{
+              translateY,
+            }}
           />
         </div>
         <div className="flex gap-2 justify-center mt-10">
